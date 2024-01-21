b0bc0aa8b92fb369ef5be9587db04da42da723c3
Ali TaghiAghdir <alitaghiaghdir@gmail.com>
2024-01-21 09:34:04 +0330

    Create first message for all caller

--- SiteSupporter.WebApp/Hubs/ChatHub.cs
+++ SiteSupporter.WebApp/Hubs/ChatHub.cs
@@ -5,13 +5,19 @@ namespace SiteSupporter.WebApp.Hubs
 {
     public class ChatHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", "Support", "Hello! What can we help you?", DateTimeOffset.UtcNow);
+            await base.OnConnectedAsync();
+        }
+
         public async Task SendMessage(string name, string message)
         {
             var chatMessage = new ChatMessage()
             {
                 FullName = name,
                 MessageBody = message,
-                Time = DateTime.Now
+                Time = DateTimeOffset.UtcNow
             };
             await Clients.All.SendAsync("ReceiveMessage", chatMessage.FullName, chatMessage.MessageBody, chatMessage.Time);
         }
